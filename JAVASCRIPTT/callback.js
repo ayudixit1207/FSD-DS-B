4492e395398bfe4cc0c39b9dc73994ff7ea3b534
Ayush Dixit <ad12cricket@gmail.com>
2026-08-21 13:59:34 +0530

    callback function

---
+++ JAVASCRIPTT/callback.js
@@ -0,0 +1,11 @@
+function sum(a,b) {
+    return a+b;
+}
+
+function sumWithMsg() {
+    const ressult=clbk(20,30);
+    const fresult="HI"+msg+"your add is="+result;
+    console.log(fresult);
+
+}
+sumWithMsg(sum)
